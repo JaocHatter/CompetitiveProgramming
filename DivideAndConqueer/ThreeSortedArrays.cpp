2034a9ff0030b2a73c710d0f70e2a3c030e4a98c
Jared Orihuela Contreras <112034917+JaocHatter@users.noreply.github.com>
2023-08-22 10:51:01 -0500

    Add files via upload

--- DivideAndConqueer/ThreeSortedArrays.cpp
+++ DivideAndConqueer/ThreeSortedArrays.cpp
@@ -3,21 +3,27 @@
 #define pb push_back
 #define ll long long
 using namespace std;
-int binarySearch(const long long arr[],int left,int right,int size, long long target) {
-    int mid = left + (right - left) / 2;
-    while (left <= right) {
-        mid = left + (right - left) / 2;
-        if (arr[mid] == target) {
+int binarySearch(const long long arr[],int left,int right,long long target,bool mod) {
+    int mid=left+(right-left)/2;
+    while (left<=right) {
+        mid=left+(right-left)/2;
+        if(arr[mid] == target) {
             return mid; // Return the index where the element was found
-        } else if (arr[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
+        }else if(arr[mid]<target) {
+            left=mid+1;
+        }else{
+            right=mid-1;
         }
     }
-    //if(mid==0 && arr[mid]!=target) return -1;
-    if(mid==size-1 && arr[mid]>target) return mid+1;
-    return mid; // Return -1 if the element was not found
+    //esto es una especie de upperbound y lowerbound dependiendo el valor de mod
+    if(mod){
+        if(target>arr[mid]) return mid+1; //first element greater than target
+        else return mid;
+    }else{
+        if(target>arr[mid]) return mid; //first element less than target
+        else return mid-1;
+    }
+    return mid;
 }
 int main(){
     int t;
@@ -35,13 +41,18 @@ int main(){
         cin>>r;
         ll t[r];
         forn(k,r) cin>>t[k];
+        //creo una matriz temporal que me ayude a guardar
+        /*
+        -el numero del array Q
+        -cuantos R[k] son mayores a Q[j]
+        -indice
+        */
         vector<vector<ll>> temp;
         forn(i,q){
             if(i>=r){
-                break;
+                break; //
             }
-            int idx=binarySearch(t,i,r-1,r,s[i]);
-            cout<<idx<<endl;
+            int idx=binarySearch(t,i,r-1,s[i],1);
             if(idx==r){
                 break;
             }
@@ -50,19 +61,14 @@ int main(){
             }
         }
         //debug
-        for(vector<ll> x: temp){
+        /*for(vector<ll> x: temp){
             cout<<x[0]<<" "<<x[1]<<" "<<x[2]<<endl;
-        }
-        //now, count how many numbers in P are less or equal than Q[j]
+        }*/
+        //now, count how many numbers in P are less or equal than TEMP[j]
         forn(i,temp.size()){
-            int idx=binarySearch(f,0,p-1,p,temp[i][0]);
-            if(f[idx]==temp[i][0] && idx<=temp[i][2]){
-                rpt+=idx*1ll*temp[i][1];
-            }else if(f[idx]>temp[i][0]){
-                if(idx==0) continue;
-                else if(idx-1<=temp[i][2]){
-                    rpt+=(idx-1)*1ll*temp[i][1];
-                }
+            int idx=binarySearch(f,0,temp[i][2],temp[i][0],0);
+            if(idx<=temp[i][2] && f[idx]<=temp[i][0]){
+                rpt+=(idx+1)*1ll*temp[i][1];
             }
         }
         cout<<rpt<<endl;
